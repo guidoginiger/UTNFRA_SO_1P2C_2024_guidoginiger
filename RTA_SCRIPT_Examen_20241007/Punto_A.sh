5b39a5853616d30e0335cad3851e0a908421e3d7
guidoginiger <ginigerguido@gmail.com>
2024-10-08 03:10:35 +0000

    ADD: Resuelto Punto_A

---
+++ RTA_SCRIPT_Examen_20241007/Punto_A.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Creo estructura"
+
+sudo mkdir -p /Examenes-UTN/{alumno_1/{parcial_1,parcial_2,parcial_3},alumno_2/{parcial_1,parcial_2,parcial_3},alumno_3/{parcial_1,parcial_2,parcial_3},profesores}
+
+echo
+echo"Muestro estructura: "
+tree /Examenes-UTN/
+
