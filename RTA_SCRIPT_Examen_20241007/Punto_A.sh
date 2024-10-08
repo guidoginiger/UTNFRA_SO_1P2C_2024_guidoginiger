bc210cb9df90b64948c719d1b8fc66ea1b648997
guidoginiger <ginigerguido@gmail.com>
2024-10-08 22:27:37 +0000

    FEAT: Modifico acceso a los permisos de ejecucion en los scripts

--- RTA_SCRIPT_Examen_20241007/Punto_A.sh
+++ RTA_SCRIPT_Examen_20241007/Punto_A.sh
@@ -5,6 +5,6 @@ echo "Creo estructura"
 sudo mkdir -p /Examenes-UTN/{alumno_1/{parcial_1,parcial_2,parcial_3},alumno_2/{parcial_1,parcial_2,parcial_3},alumno_3/{parcial_1,parcial_2,parcial_3},profesores}
 
 echo
-echo"Muestro estructura: "
+echo "Muestro estructura: "
 tree /Examenes-UTN/
 
