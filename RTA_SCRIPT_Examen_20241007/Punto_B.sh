d33b1008124b8c1814bc38549d91c06b95a01166
guidoginiger <ginigerguido@gmail.com>
2024-10-09 04:27:36 +0000

    ADD: Agrego Montaje Persistente.

--- RTA_SCRIPT_Examen_20241007/Punto_B.sh
+++ RTA_SCRIPT_Examen_20241007/Punto_B.sh
@@ -4,6 +4,7 @@
 # Obtengo el DISCO = sudo fdisk -l |grep "10GiB" | awk '{print $2}' | awk -F ':' '{print $1}
 
 DISCO=$(sudo fdisk -l |grep "10 GiB" | awk '{print $2}' | awk -F ':' '{print $1}')
+RTA=$(grep -c $DISCO /etc/fstab)
 
 echo "Particion del disco: $DISCO"
 echo
@@ -37,5 +38,19 @@ sudo mkfs.ext4 ${DISCO}5
 echo
 
 echo "Montaje PERSISTENTE"
-sudo mount ${DISCO}1 
-sudo mount ${DISCO}5
+
+if [[$RTA == 0]]; then
+	echo "${DISCO}1 /Examenes-UTN/alumno_1/parcial_1 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+	echo "${DISCO}1 /Examenes-UTN/alumno_1/parcial_2 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+	echo "${DISCO}1 /Examenes-UTN/alumno_1/parcial_3 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+	echo "${DISCO}1 /Examenes-UTN/alumno_2/parcial_1 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+	echo "${DISCO}1 /Examenes-UTN/alumno_2/parcial_2 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+	echo "${DISCO}1 /Examenes-UTN/alumno_2/parcial_3 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+	echo "${DISCO}1 /Examenes-UTN/alumno_3/parcial_1 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+	echo "${DISCO}1 /Examenes-UTN/alumno_3/parcial_2 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+	echo "${DISCO}1 /Examenes-UTN/alumno_3/parcial_3 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+	echo "${DISCO}1 /Examenes-UTN/profesores ext4 defaults 0 0" | sudo tee -a /etc/fstab
+
+fi
+
+sudo mount -a
