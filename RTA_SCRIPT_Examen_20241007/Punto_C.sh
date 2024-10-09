23cd79f0ed582d8a54ccaf3f7a1086d53b9ad46a
guidoginiger <ginigerguido@gmail.com>
2024-10-09 19:42:14 +0000

    ADD: Punto_C.sh

--- RTA_SCRIPT_Examen_20241007/Punto_C.sh
+++ RTA_SCRIPT_Examen_20241007/Punto_C.sh
@@ -2,13 +2,14 @@
 
 #HASH=$(sudo grep -i $USUARIO_HASH /etc/shadow | awk -F ':' '{print $2}')
 
+
 #echo "Creo Grupos"
 sudo groupadd p1c2_2024_gAlumno
 sudo groupadd p1c2_2024_gProfesores
 
 #echo "Creo Usuarios"
-sudo useradd -m -s /bin/bash -p sudo grep -i p1c2_2024_gAlumno p1c2_2024_A1 /etc/shadow | awk -F ':' '{print $2} -G p1c2_2024_gAlumno p1c2_2024_A1
-sudo useradd -m -s /bin/bash -G p1c2_2024_gAlumno p1c2_2024_A2
-sudo useradd -m -s /bin/bash -G p1c2_2024_gAlumno p1c2_2024_A3
-sudo useradd -m -s /bin/bash -G p1c2_2024_gProfesores p1c2_2024_P1
+sudo useradd -m -s /bin/bash -G p1c2_2024_gAlumno -p "$(sudo grep osboxes /etc/shadow | awk -F ':' '{print $2}')" p1c2_2024_A1
+sudo useradd -m -s /bin/bash -G p1c2_2024_gAlumno -p "$(sudo grep osboxes /etc/shadow | awk -F ':' '{print $2}')" p1c2_2024_A2
+sudo useradd -m -s /bin/bash -G p1c2_2024_gAlumno -p "$(sudo grep osboxes /etc/shadow | awk -F ':' '{print $2}')" p1c2_2024_A3
+sudo useradd -m -s /bin/bash -G p1c2_2024_gProfesores -p "$(sudo grep osboxes /etc/shadow | awk -F ':' '{print $2}')" p1c2_2024_P1
 
