3f632ee8fd6de7e4d520397aec5fa86398582996
guidoginiger <ginigerguido@gmail.com>
2024-10-10 01:40:01 +0000

    FIX: Corrijo Punto_D.sh ADD: Agrego el historial de comandos

--- RTA_SCRIPT_Examen_20241007/Punto_D.sh
+++ RTA_SCRIPT_Examen_20241007/Punto_D.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # Estructura Asimetrica
-mkdir -p ~/Estructura_Asimetrica/correo ~/Estructura_Asimetrica/cliente
+sudo mkdir -p /home/vagrant/Estructura_Asimetrica/{correo/{cartas_{1..100},cartero_{1..10}},cliente/cartas_{1..100}}
 
+#DA ERROR
 # touch
-touch ~/Estructura_Asimetrica/correo/cartas_{1..100}
-touch ~/Estructura_Asimetrica/clientes/cartas_{1..100}
-touch ~/Estructura_Asimetrica/correo/carteros_{1..10}
+#touch ~/Estructura_Asimetrica/correo/cartas_{1..100}
+#touch ~/Estructura_Asimetrica/clientes/cartas_{1..100}
+#touch ~/Estructura_Asimetrica/correo/carteros_{1..10}
 
 # Validacion
 tree ~/Estructura_Asimetrica/ --noreport | pr -T5 -W80 --column=4
