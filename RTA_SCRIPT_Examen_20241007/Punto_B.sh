96e08b5852167e3f58e211d8f74a187903697bb4
guidoginiger <ginigerguido@gmail.com>
2024-10-10 01:11:26 +0000

    FIX: Corrijo y completo Punto_B.sh

--- RTA_SCRIPT_Examen_20241007/Punto_B.sh
+++ RTA_SCRIPT_Examen_20241007/Punto_B.sh
@@ -23,8 +23,33 @@ e
 n
 
 +900M
+n
+
++900M
+n
+
++900M
+n
+
++900M
+n
+
++900M
+n
+
++900m
+n
+
++900M
+n
+
++900M
+n
+
+
 w
 EOF
+
 echo
 
 echo "Muestro como quedo: "
@@ -35,21 +60,29 @@ echo "Formateo particiones: "
 
 sudo mkfs.ext4 ${DISCO}1
 sudo mkfs.ext4 ${DISCO}5
+sudo mkfs.ext4 ${DISCO}6
+sudo mkfs.ext4 ${DISCO}7
+sudo mkfs.ext4 ${DISCO}8
+sudo mkfs.ext4 ${DISCO}9
+sudo mkfs.ext4 ${DISCO}10
+sudo mkfs.ext4 ${DISCO}11
+sudo mkfs.ext4 ${DISCO}12
+sudo mkfs.ext4 ${DISCO}13
 echo
 
 echo "Montaje PERSISTENTE"
 
 if [[$RTA == 0]]; then
-	echo "${DISCO}1 /Examenes-UTN/alumno_1/parcial_1 ext4 defaults 0 0" | sudo tee -a /etc/fstab
-	echo "${DISCO}1 /Examenes-UTN/alumno_1/parcial_2 ext4 defaults 0 0" | sudo tee -a /etc/fstab
-	echo "${DISCO}1 /Examenes-UTN/alumno_1/parcial_3 ext4 defaults 0 0" | sudo tee -a /etc/fstab
-	echo "${DISCO}1 /Examenes-UTN/alumno_2/parcial_1 ext4 defaults 0 0" | sudo tee -a /etc/fstab
-	echo "${DISCO}1 /Examenes-UTN/alumno_2/parcial_2 ext4 defaults 0 0" | sudo tee -a /etc/fstab
-	echo "${DISCO}1 /Examenes-UTN/alumno_2/parcial_3 ext4 defaults 0 0" | sudo tee -a /etc/fstab
-	echo "${DISCO}1 /Examenes-UTN/alumno_3/parcial_1 ext4 defaults 0 0" | sudo tee -a /etc/fstab
-	echo "${DISCO}1 /Examenes-UTN/alumno_3/parcial_2 ext4 defaults 0 0" | sudo tee -a /etc/fstab
-	echo "${DISCO}1 /Examenes-UTN/alumno_3/parcial_3 ext4 defaults 0 0" | sudo tee -a /etc/fstab
-	echo "${DISCO}1 /Examenes-UTN/profesores ext4 defaults 0 0" | sudo tee -a /etc/fstab
+	${DISCO}1 /Examenes-UTN/alumno_1/parcial_1 ext4 defaults 0 0 | sudo tee -a /etc/fstab
+	${DISCO}5 /Examenes-UTN/alumno_1/parcial_2 ext4 defaults 0 0 | sudo tee -a /etc/fstab
+	${DISCO}6 /Examenes-UTN/alumno_1/parcial_3 ext4 defaults 0 0 | sudo tee -a /etc/fstab
+	${DISCO}7 /Examenes-UTN/alumno_2/parcial_1 ext4 defaults 0 0 | sudo tee -a /etc/fstab
+	${DISCO}8 /Examenes-UTN/alumno_2/parcial_2 ext4 defaults 0 0 | sudo tee -a /etc/fstab
+	${DISCO}9 /Examenes-UTN/alumno_2/parcial_3 ext4 defaults 0 0 | sudo tee -a /etc/fstab
+	${DISCO}10 /Examenes-UTN/alumno_3/parcial_1 ext4 defaults 0 0 | sudo tee -a /etc/fstab
+	${DISCO}11 /Examenes-UTN/alumno_3/parcial_2 ext4 defaults 0 0 | sudo tee -a /etc/fstab
+	${DISCO}12 /Examenes-UTN/alumno_3/parcial_3 ext4 defaults 0 0 | sudo tee -a /etc/fstab
+	${DISCO}13 /Examenes-UTN/profesores ext4 defaults 0 0 | sudo tee -a /etc/fstab
 
 fi
 
